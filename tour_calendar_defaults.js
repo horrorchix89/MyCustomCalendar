8450a93bb96df729eafc90baff81d0501952ac00
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-01-10 05:38:29 -0600

    Create tour_calendar_defaults.js

---
+++ tour_calendar_defaults.js
@@ -0,0 +1,31 @@
+var display_calendar_options = {
+header: {
+      left: "today prev,next title",
+      center: "",
+      right: ""
+    },
+    aspectRatio: 1.8,
+    fixedWeekCount: false,
+    nowIndicator: true,
+    slotLabelInterval: "01:00",
+    slotLabelFormat: "h A",
+    listDayFormat: "ddd MMM D",
+    listDayAltFormat: false,
+    noEventsMessage: "no events",
+    buttonText: {
+        prev: 'prev',
+        next: 'next',
+        prevYear: 'prev year',
+        nextYear: 'next year',
+        year: 'year',
+        today: 'today',
+        month: 'month',
+        week: 'week',
+        day: 'day'
+    },
+    buttonIcons: {
+        prev: 'right-single-arrow',
+        next: 'left-single-arrow',
+        prevYear: 'right-double-arrow',
+        nextYear: 'left-double-arrow'
+    },
