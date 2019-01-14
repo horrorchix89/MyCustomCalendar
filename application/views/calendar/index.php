6560591cfeea9abcaf403519baff67ce52e2b585
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-01-14 05:36:42 -0600

    Update index.php

--- application/views/calendar/index.php
+++ application/views/calendar/index.php
@@ -5,6 +5,10 @@
         <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <link rel="stylesheet" href="<?php echo base_url() ?>scripts/fullcalendar/fullcalendar.min.css" />
+               <script src="<?php echo base_url() ?>scripts/fullcalendar/lib/moment.min.js"></script>
+               <script src="<?php echo base_url() ?>scripts/fullcalendar/fullcalendar.min.js"></script>
+               <script src="<?php echo base_url() ?>scripts/fullcalendar/gcal.js"></script>
     </head>
     <body>
 
