d0e26f4a263bdb349a9d21e7db25b3bded2c3265
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-01-14 05:35:02 -0600

    Update index.php

--- application/views/calendar/index.php
+++ application/views/calendar/index.php
@@ -1,1 +1,30 @@
-placeholder file
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Calendar Display</title>
+        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    </head>
+    <body>
+
+    <div class="container">
+    <div class="row">
+    <div class="col-md-12">
+
+    <h1>Caelndar</h1>
+
+
+
+    </div>
+    </div>
+    </div>
+
+
+<script type="text/javascript">
+$(document).ready(function() {
+
+});
+</script>
+    </body>
+</html>
