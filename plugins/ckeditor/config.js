6cb5d79dce4bacdec13d0eba5de4b48d540fca70
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-02 16:26:49 -0600

    new stuff

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-	config.height = 300;
+	config.height = 125;
 	config.toolbarCanCollapse = true;
 
 	// The toolbar groups arrangement, optimized for a single toolbar row.
