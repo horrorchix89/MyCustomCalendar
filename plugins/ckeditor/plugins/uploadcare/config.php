4e9e99ae2e9358752bfd25601983157c0327b216
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-02 21:54:24 -0600

    Update config.php

--- plugins/ckeditor/plugins/uploadcare/config.php
+++ plugins/ckeditor/plugins/uploadcare/config.php
@@ -1,3 +1,3 @@
 <?php
-define('UC_PUBLIC_KEY', 'demopublickey');
-define('UC_SECRET_KEY', 'demoprivatekey');
+define('UC_PUBLIC_KEY', 'b56927a6dd79d009ab8c');
+define('UC_SECRET_KEY', '06ba401de36b11d7e17b');
