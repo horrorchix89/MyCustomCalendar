9b6c60a75b472758ed4e47c2252f02c92a8bce81
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-12 15:43:05 -0600

    Update config.js

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -4,9 +4,11 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	config.height = 125;
+	config.height = 150;
 	config.colorButton_colorsPerRow = 8;
 	config.colorButton_enableMore = false;
+	config.colorButton_colors = '000000/Black,A0522D/Sienna,556B2F/DarkOliveGreen,006400/DarkGreen,483D8B/DarkSlateBlue,000080/Navy,4B0082/Indigo,2F4F4F/DarkSlateGray,' + '8B0000/DarkRed,FF8C00/DarkOrange,808000/Olive,008000/Green,008080/Teal,0000FF/Blue,708090/SlateGray,696969/DimGray,' 
++ 'FF0000/Red,F4A460/SandyBrown,9ACD32/YellowGreen,2E8B57/SeaGreen,48D1CC/MediumTurquoise,4169E1/RoyalBlue,800080/Purple,808080/Gray,' + 'FF00FF/Magenta,FFA500/Orange,FFFF00/Yellow,00FF00/Lime,00FFFF/Cyan,00BFFF/DeepSkyBlue,9932CC/DarkOrchid,C0C0C0/Silver,' + 'FFC0CB/Pink,F5DEB3/Wheat,FFFACD/LemonChiffon,98FB98/PaleGreen,AFEEEE/PaleTurquoise,ADD8E6/LightBlue,DDA0DD/Plum,FFFFFF/White';
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
