a675a743d485957bce1b4bbeddddd5e80116ac8f
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-12 22:29:41 -0600

    ckeditor

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -2,39 +2,71 @@
  * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
+CKEDITOR.editorConfig = function (config) {
+   // Define changes to default configuration here.
+   // For complete reference see:
+   // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-CKEDITOR.editorConfig = function( config ) {
-	config.height = 150;
-	config.colorButton_colorsPerRow = 8;
-	config.colorButton_enableMore = false;
-	config.colorButton_colors =
-    '000,800000,8B4513,2F4F4F,008080,000080,4B0082,696969,' +
-    'B22222,A52A2A,DAA520,006400,40E0D0,0000CD,800080,808080,' +
-    'F00,FF8C00,FFD700,008000,0FF,00F,EE82EE,A9A9A9,' +
-    'FFA07A,FFA500,FFFF00,00FF00,AFEEEE,ADD8E6,DDA0DD,D3D3D3,' +
-    'FFF0F5,FAEBD7,FFFFE0,F0FFF0,F0FFFF,F0F8FF,E6E6FA,FFF';
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+   // The toolbar groups arrangement, optimized for a single toolbar row.
+   config.height = 150;
+   config.colorButton_colorsPerRow = 8;
+   config.colorButton_enableMore = false;
+   config.colorButton_colors =
+      '000,800000,8B4513,2F4F4F,008080,000080,4B0082,696969,' +
+      'B22222,A52A2A,DAA520,006400,40E0D0,0000CD,800080,808080,' +
+      'F00,FF8C00,FFD700,008000,0FF,00F,EE82EE,A9A9A9,' +
+      'FFA07A,FFA500,FFFF00,00FF00,AFEEEE,ADD8E6,DDA0DD,D3D3D3,' +
+      'FFF0F5,FAEBD7,FFFFE0,F0FFF0,F0FFFF,F0F8FF,E6E6FA,FFF';
+   config.toolbarGroups = [{
+         name: 'document',
+         groups: ['mode', 'document', 'doctools']
+      },
+      {
+         name: 'clipboard',
+         groups: ['clipboard', 'undo']
+      },
+      {
+         name: 'editing',
+         groups: ['find', 'selection', 'spellchecker']
+      },
+      {
+         name: 'forms'
+      },
+      {
+         name: 'basicstyles',
+         groups: ['basicstyles', 'cleanup']
+      },
+      {
+         name: 'paragraph',
+         groups: ['list', 'indent', 'blocks', 'align', 'bidi']
+      },
+      {
+         name: 'links'
+      },
+      {
+         name: 'insert'
+      },
+      {
+         name: 'styles'
+      },
+      {
+         name: 'colors'
+      },
+      {
+         name: 'tools'
+      },
+      {
+         name: 'others'
+      },
+      {
+         name: 'about'
+      }
+   ];
 
-	// The toolbar groups arrangement, optimized for a single toolbar row.
-	config.toolbarGroups = [
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'clipboard', groups: [ 'undo', 'clipboard' ] },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'align', 'list', 'indent', 'blocks', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
+   // The default plugins included in the basic setup define some buttons that
+   // are not needed in a basic editor. They are removed here.
+   config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
 
-	config.removeButtons = 'BGColor,Subscript,Superscript,Anchor,JustifyBlock,Outdent,Indent,Font,FontSize,Uploadcare,Table';
-	// Dialog windows are also simplified.
-	config.removeDialogTabs = 'link:advanced';
+   // Dialog windows are also simplified.
+   config.removeDialogTabs = 'link:advanced';
 };
