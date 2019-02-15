ab47785de97955b005c48ffdff68a82241e52110
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-15 00:49:13 -0600

    Update config.js

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -4,22 +4,22 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-    config.height = '150px';
+	config.height = '125px';
 	config.toolbarGroups = [
-		{ name: 'clipboard', groups: [ 'undo', 'clipboard' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'indent', 'blocks', 'align', 'list', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
+            { name: 'clipboard', groups: [ 'undo', 'clipboard' ] },
+	    { name: 'forms', groups: [ 'forms' ] },
+	    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+	    { name: 'paragraph', groups: [ 'indent', 'blocks', 'align', 'list', 'bidi', 'paragraph' ] },
+            { name: 'links', groups: [ 'links' ] },
+            { name: 'insert', groups: [ 'insert' ] },
+            { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+            { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+            { name: 'styles', groups: [ 'styles' ] },
+            { name: 'colors', groups: [ 'colors' ] },
+            { name: 'tools', groups: [ 'tools' ] },
+            { name: 'others', groups: [ 'others' ] },
+            { name: 'about', groups: [ 'about' ] }
+        ];
 
 	config.removeButtons = 'Cut,Copy,Paste,Anchor,Subscript,Superscript,Outdent,Indent,JustifyBlock';
 };
