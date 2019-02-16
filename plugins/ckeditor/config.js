fc0b98a3b627463f59e9672ad1ddbd48b9b33efe
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-15 23:51:34 -0600

    Update config.js

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -4,7 +4,14 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+    config.extraAllowedContent =
+        'a abbr acronym address area article aside b base basefont bdi bdo big blink blockquote ' +
+        'br button caption center cite code col colgroup content data datalist dd del details dfn dir div dl dt ' +
+        'element em fieldset figcaption figure font footer h1 h2 h3 h4 h5 h6 header hgroup hr i img ins label legend ' +
+        'li link listing main map mark marquee menu menuitem meta meter nav nobr ol p pre progress q s samp section small ' +
+        'spacer span strike strong style sub summary sup table tbody td textarea tfoot th thead tr tt u ul wbr xmp(*){*}[*]';
     config.extraPlugins = 'ckawesome';
+    config.disableNativeSpellChecker = false;
     config.height = '100px';
     config.toolbar = [
         { name: 'CKAwesome', items: [ 'Image', 'ckawesome' ] }
