8d424efdf54c9a5c037efb479e51ba7d714882ae
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-17 02:11:59 -0600

    Update config.js

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -15,22 +15,14 @@ CKEDITOR.editorConfig = function( config ) {
     config.height = '100px';
     config.toolbarCanCollapse = true;
 	config.toolbarGroups = [
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'forms' },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'tools' },
-		{ name: 'others' },
-		{ name: 'about' }
+		{ name: 'paragraph', groups: [ 'align', 'list', 'indent' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'editing', groups: [ 'find' ] },
+		{ name: 'document', groups: [ 'document', 'mode' ] }
 	];
 
-	// The default plugins included in the basic setup define some buttons that
-	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+	config.removeButtons = 'Cut,Copy,Paste,Anchor,Subscript,Superscript,JustifyBlock,Outdent,Indent';
 };
