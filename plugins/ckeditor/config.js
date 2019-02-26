98f4be93fd916eec6891e13c18184413a13035fc
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-02-25 18:01:33 -0600

    datepicker and ckeeditor changes

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -12,22 +12,23 @@ CKEDITOR.editorConfig = function( config ) {
     config.autoGrow_maxHeight = 300;
     config.disableNativeSpellChecker = false;
     config.format_tags = 'p;h1;h2;h3;h4;h5;h6;div';
+    config.language = 'en';
     config.height = 150;
     config.width = '100%';
     config.toolbarCanCollapse = true;
 
 	// The toolbar groups arrangement, optimized for a single toolbar row.
 	config.toolbar = [
-		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Strike', 'Underline' ] },		
 		{ name: 'styles', items: [ 'Format' ] },
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Strike', 'Underline' ] },
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste' ] },
 		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList' ] },
 		{ name: 'paragraph', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
-		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'RemoveFormat' ] },
 		{ name: 'undo', items: [ 'Undo', 'Redo' ] },
 		{ name: 'links', items: [ 'Link', 'Unlink', 'HorizontalRule' ] },
-		{ name: 'document', items: [ 'Source' ] },
 		{ name: 'editing', items: [ 'Find', 'Replace' ] },
-		{ name: 'insert', items: [ 'ckawesome', 'EmojiPanel' ] }
+		{ name: 'insert', items: [ 'ckawesome', 'EmojiPanel' ] },
+		{ name: 'document', items: [ 'RemoveFormat', 'Source' ] }
 	];
 
 	config.removeButtons = 'Anchor,Subscript,Superscript,Outdent,Indent,JustifyBlock';
