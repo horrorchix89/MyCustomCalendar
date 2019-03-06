bb29c17db890d668fd86c0de305c2d838ea47b5e
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-03-05 21:00:18 -0600

    Update config.js
    Edited toolbar

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -3,25 +3,20 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function(config) {
+	config.allowedContent = true;
 	config.disableNativeSpellChecker = false;
 	config.format_tags = 'p;h1;h2;h3;h4;h5;h6;div';
-	config.height = 125;
+	config.height = 100;
 	config.toolbarCanCollapse = true;
-	config.toolbar = [
-		{ name: 'undo', items: [ 'Undo', 'Redo' ] },
-		{ name: 'styles', items: [ 'Format' ] },
-		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Strike', 'Underline' ] },
-        { name: 'paragraph', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
-		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList' ] },
-		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste' ] },
-		{ name: 'links', items: [ 'Link', 'Unlink', 'HorizontalRule' ] },
-		{ name: 'editing', items: [ 'Find', 'Replace' ] },
-		{ name: 'insert', items: [ 'ckawesome', 'EmojiPanel' ] },
-		{ name: 'document', items: [ 'RemoveFormat', 'Source' ] }
+	config.toolbar = [ 
+		'Undo', 'Redo', 'Format', 'Bold', 'Italic', 'Strike', 'Underline', 
+		'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'NumberedList', 
+		'BulletedList', 'Cut', 'Copy', 'Paste', 'Link', 'Unlink', 'HorizontalRule',
+		'Find', 'Replace', 'ckawesome', 'EmojiPanel', 'RemoveFormat', 'Source'
 	];
 
 	config.removeButtons = 'Anchor,Subscript,Superscript,Outdent,Indent,JustifyBlock';
 	config.removeDialogTabs = '';
-    config.removePlugins = 'autogrow';
+	config.removePlugins = 'autogrow';
 };
