20b15e83ecd6019ff992fd046339776801c35c82
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-03-05 21:04:10 -0600

    Update config.js
    Toolbar edit

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -9,11 +9,17 @@ CKEDITOR.editorConfig = function(config) {
 	config.format_tags = 'p;h1;h2;h3;h4;h5;h6;div';
 	config.height = 100;
 	config.toolbarCanCollapse = true;
-	config.toolbar = [ 
-		'Undo', 'Redo', 'Format', 'Bold', 'Italic', 'Strike', 'Underline', 
-		'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'NumberedList', 
-		'BulletedList', 'Cut', 'Copy', 'Paste', 'Link', 'Unlink', 'HorizontalRule',
-		'Find', 'Replace', 'ckawesome', 'EmojiPanel', 'RemoveFormat', 'Source'
+	config.toolbar = [
+		{ name: 'undo', items: [ 'Undo', 'Redo' ] },
+		{ name: 'styles', items: [ 'Format' ] },
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Strike', 'Underline' ] },
+		{ name: 'paragraph', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
+		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList' ] },
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'HorizontalRule' ] },
+		{ name: 'editing', items: [ 'Find', 'Replace' ] },
+		{ name: 'insert', items: [ 'ckawesome', 'EmojiPanel' ] },
+		{ name: 'document', items: [ 'RemoveFormat', 'Source' ] }
 	];
 
 	config.removeButtons = 'Anchor,Subscript,Superscript,Outdent,Indent,JustifyBlock';
