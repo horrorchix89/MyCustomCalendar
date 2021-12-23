61a5b01b8ded2eb06758f45b59167902d3d5090d
Elizabeth <horrorchix89@gmail.com>
2021-12-23 16:33:54 -0600

    Config edits

--- plugins/ckeditor/config.js
+++ plugins/ckeditor/config.js
@@ -8,18 +8,19 @@ CKEDITOR.editorConfig = function(config) {
     config.coreStyles_italic = { element: 'em' };
     config.disableNativeSpellChecker = false;
     config.extraAllowedContent = "a abbr acronym address area article aside b base basefont bdi bdo big blink blockquote body br button canvas caption center cite code col colgroup content data datalist dd del details dfn dialog dir div dl dt element em fieldset figcaption figure font footer form h1 h2 h3 h4 h5 h6 header hgroup hr html i iframe img input ins label legend li link listing main map mark marquee menu menuitem meta meter nav nobr object ol optgroup output p picture pre progress q s samp script section select small spacer span strike strong style sub summary sup table text-align tbody td textarea tfoot th thead time title tr tt u ul var video wbr xmp(*){*}[*]";
-    //config.fillEmptyBlocks = false;
+    config.fillEmptyBlocks = false;
     config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;32/32px;36/36px;48/48px';
     config.forceSimpleAmpersand = true;
     config.format_tags = 'p;h1;h2;h3;h4;h5;h6;div;formatted';
-    config.height = 96;
+    config.height = 110;
     config.ignoreEmptyParagraph = false;
+  	config.language = 'en';
     config.pasteTools_keepZeroMargins = true;
     config.pasteFilter = null;
     config.pasteFromWordRemoveStyles = false;
     config.resize_enabled = false;
     config.sourceAreaTabSize = 2;
-    config.tabSpaces = 2;
+    //config.tabSpaces = 2;
     config.toolbarCanCollapse = true;
     config.toolbar = [
         { name: 'undo', items: [ 'Undo', 'Redo' ] },
@@ -32,5 +33,5 @@ CKEDITOR.editorConfig = function(config) {
     ];
     config.removeButtons = 'Anchor,Subscript,Superscript,Outdent,Indent,JustifyBlock,PasteFromWord,Font,FontSize,Save,Cut,Copy,Paste';
     config.removeDialogTabs = 'link:advanced';
-    config.removePlugins = 'ckawesome,resize';
+    config.removePlugins = 'ckawesome';
 };
